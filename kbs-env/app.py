bc5476ecf509ca2fa9cd01c80724a65c67445a73
syarifahaisha <aishageubrina@gmail.com>
2022-11-03 23:03:59 +0700

    done

--- kbs-env/app.py
+++ kbs-env/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 def load_model():
     classifier_from_pickle = pickle.load(open('../model.pkl', 'rb'))
-    print(classifier_from_pickle)
+    # print(classifier_from_pickle)
     return classifier_from_pickle
 
 def preprocess_input(input_dict:dict):
@@ -18,8 +18,9 @@ def preprocess_input(input_dict:dict):
 
     #Remove unecessary column
     df = df.drop('id', axis=1)
+    df = df.drop('stroke', axis=1)
 
-    df_input = pd.DataFrame.from_dict([dict], orient='columns')
+    df_input = pd.DataFrame.from_dict([input_dict], orient='columns')
     
     df = pd.concat([df, df_input])
 
@@ -30,18 +31,27 @@ def preprocess_input(input_dict:dict):
     df['Residence_type'] = le.fit_transform(df['Residence_type'])
     df['smoking_status'] = le.fit_transform(df['smoking_status'])
 
-    print(df[:10])
-
     return df.tail(1).values.tolist()
 
+def convert_output(class_pred, confidence_val):
+    print("Class", class_pred)
+    conv_class_pred = "Stroke" if (class_pred == 1) else "Not Stroke"
+    conv_confidence_val = "{0:.2f}%".format(confidence_val * 100)
+
+    return conv_class_pred, conv_confidence_val
+
 def predict(classifier, input):
+    print(input)
     y_pred = classifier.predict(input)
 
-    return y_pred, classifier.predict_proba(input).max()
+    class_pred, confidence_val = convert_output(y_pred[0], classifier.predict_proba(input).max())
 
-@app.route('/', methods=['GET', 'POST'])
+    return class_pred, confidence_val
+
+@app.route('/', methods=('GET', 'POST'))
 def index():
     if (request.method == 'POST'):
+
         input_raw = {}
         input_raw["gender"] = request.values.get('gender')
         input_raw["age"] = request.values.get('age')
@@ -49,12 +59,10 @@ def index():
         input_raw["heart_disease"] = request.values.get('heart_disease')
         input_raw["ever_married"] = request.values.get('ever_married')
         input_raw["work_type"] = request.values.get('work_type')
-        input_raw["residence_type"] = request.values.get('residence_type')
-        input_raw["avg_glukose_level"] = request.values.get('avg_glukose_level')
+        input_raw["Residence_type"] = request.values.get('residence_type')
+        input_raw["avg_glucose_level"] = request.values.get('avg_glukose_level')
         input_raw["bmi"] = request.values.get('bmi')
         input_raw["smoking_status"] = request.values.get('smoking_status')
-
-        print(input_raw)
 
         input = preprocess_input(input_raw)
 
